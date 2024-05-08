af741197a069e99bdd80b56f27acff9e37fd9964
Ludmila (DeFinn) <ludmila.omlopes@gmail.com>
2024-05-08 20:43:00 -0300

    search component

--- components/nav.tsx
+++ components/nav.tsx
@@ -7,13 +7,15 @@ import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import { ModeToggle } from '@/components/dropdown';
 import { ChevronRight, Droplets, LogOut } from "lucide-react";
-import { useCallback } from 'react';
+import { useCallback, useState, useEffect } from 'react';
 import { Dialog, DialogTrigger, DialogContent } from '@/components/ui/dialog';
 import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
 import { Card } from '@/components/ui/card';
-import { ProfileId, useLogin, useLogout, useProfilesManaged } from '@lens-protocol/react-web';
+import { ProfileId, useLogin, useLogout, useProfilesManaged, useProfiles } from '@lens-protocol/react-web';
 import { useLoginState } from '@/app/loginStateProvider';
 import { Drawer, DrawerContent, DrawerTrigger } from '@/components/ui/drawer';
+import { debounce } from 'lodash'; // Import lodash debounce function
+import SearchProfiles from './searchProfiles';
 
 export function Nav() {
   const { open } = useWeb3Modal();
@@ -24,6 +26,8 @@ export function Nav() {
   const { execute: exLogout } = useLogout();
   const { data: managedProfiles, loading: loadingProfiles } = useProfilesManaged({ for: address ?? '' });
   const { isLoggedIn, setIsLoggedIn, profileId, setProfileId } = useLoginState();
+  const [openSearch, setOpenSearch] = useState(false);
+  const [searchInput, setSearchInput] = useState('');
 
   const login = useCallback((profileId: ProfileId) => {
     if (!address) {
@@ -50,8 +54,29 @@ export function Nav() {
     });
   }, [exLogout, setIsLoggedIn, setProfileId]);
 
+  const { data: profiles, loading: searchLoading, error: searchError } = useProfiles({
+    where: {
+      handles: searchInput ? [`lens/${searchInput}`] : [],
+    },
+  });
+
+  const search = useCallback(() => {
+    setOpenSearch((open) => !open)
+},[]);
+
+  const debouncedSearch = useCallback(debounce(setSearchInput, 300), []);
+
+  const handleSearchInput = (event) => {
+    debouncedSearch(event.target.value);
+  };
+
+  const toggleSearch = useCallback(() => {
+    setOpenSearch((open) => !open);
+  }, []);
+
   return (
     <nav className='border-b flex flex-row items-start sm:items-center sm:pr-10 px-4 sm:px-0'>
+      { /* Mobile Menu: Drawer*/ } 
       <div className='py-3 px-8 flex flex-1 items-center p sm:hidden'>
         <Drawer>
           <DrawerTrigger asChild>
@@ -71,20 +96,19 @@ export function Nav() {
             )}
           </DrawerContent>
         </Drawer>
-      </div>
+      </div>    
+    { /* Desktop Menu - Nav bar */ } 
       <div className='py-3 px-8 flex flex-1 items-center p hidden sm:flex sm:items-center'>
         <Link href="/" className='mr-5 flex items-center'>
           <Droplets className="opacity-85" size={19} />
           <p className={`ml-2 mr-4 text-lg font-semibold`}>Mystic Garden</p>
         </Link>
-  
         <Link href="/" className={`mr-5 text-sm ${pathname !== '/' && 'opacity-50'}`}>
           <p>Home</p>
         </Link>
         {/*  <Link href="/gallery" className={`mr-5 text-sm ${pathname !== '/gallery' && 'opacity-50'}`}>
           <p>Gallery</p>
         </Link> */ }
-  
         {
           address && (
             <Link href="/profile" className={`mr-5 text-sm ${pathname !== '/profile' && 'opacity-60'}`}>
@@ -92,6 +116,7 @@ export function Nav() {
             </Link>
           )
         }
+      <SearchProfiles />
       </div>
       <div className='flex sm:items-center pl-8 py-3 sm:p-0'>
         {
