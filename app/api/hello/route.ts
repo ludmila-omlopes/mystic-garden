5148f47793d9faa0e6ac26433512bf103bee482f
Ludmila (DeFinn) <ludmila.omlopes@gmail.com>
2024-05-26 19:42:45 -0300

    remove the hello route

--- app/api/hello/route.ts
+++
@@ -1,5 +0,0 @@
-export const dynamic = 'force-dynamic'; // static by default, unless reading the request
- 
-export function GET(request: Request) {
-  return new Response(`Hello from ${process.env.VERCEL_REGION}`);
-}
--- app/api/irys.ts
+++ app/api/irys.ts
@@ -2,8 +2,8 @@ import Irys from '@irys/sdk';
 import { DEVNET_PROVIDER_URL } from '../constants';
 
 export async function getIrys() {
-	const network = process.env.NEXT_PUBLIC_ENVIRONMENT === "production" ? "mainnet" : "devnet";
-	const providerUrl = process.env.NEXT_PUBLIC_ENVIRONMENT === "production" ? "" : DEVNET_PROVIDER_URL;
+	const network = process.env.NEXT_PUBLIC_IRYS_ENVIRONMENT === "production" ? "mainnet" : "devnet";
+	const providerUrl = process.env.NEXT_PUBLIC_IRYS_ENVIRONMENT === "production" ? "" : DEVNET_PROVIDER_URL;
 	const token = "matic"; //token usado pra pagar o nó do arweave
 	const privateKey = process.env.PRIVATE_KEY;
  
