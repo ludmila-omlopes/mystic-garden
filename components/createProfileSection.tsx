42948ea87ce82557fa68edc0b353500d31a33528
Ludmila (DeFinn) <ludmila.omlopes@gmail.com>
2024-08-01 17:26:18 -0300

    new onboarding section

--- components/createProfileSection.tsx
+++ components/createProfileSection.tsx
@@ -8,10 +8,10 @@ export default function HomepageCallToAction() {
     <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
       <div className="container px-4 md:px-6">
         <div className="flex flex-col items-center gap-4 text-center">
-        <LensIcon className="h-24 w-24 mb-4" />
+          <LensIcon className="h-24 w-24 mb-4" />
           <div className="space-y-2">
             <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl">
-              You don't have a Lens profile yet?
+              You don&apos;t have a Lens profile yet?
             </h2>
             <p className="mx-auto max-w-[700px] text-muted-foreground md:text-xl">
               Mystic Garden is built on top of Lens Protocol. We can guide you on how to create a new profile and join our amazing community!
